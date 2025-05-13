e62213fc3df004351b6223160a52f8311a3861c0
KoroAdiguna <bhagaskoro.aaw@gmail.com>
2025-05-13 16:05:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function tambah() {
+    let inp = document.getElementById("teks");
+    let mskInp = inp.value.trim();
+
+    if (mskInp === "") {
+        alert("Tulislah tujuan mu");
+        return;
+    }
+    
+    let contenr = document.getElementById("ok");
+    let li = document.createElement("li");
+    
+    li.id = "halo";
+    li.textContent = mskInp;
+    
+    let button = document.createElement("button");
+    button.className = "hps";
+    button.innerText = "Hapus";
+    button.style.color = "white";
+    button.style.backgroundColor = "green";
+    button.style.marginLeft = "25px";
+    
+    button.onclick = function() {
+        contenr.removeChild(li);
+    };
+    
+    li.appendChild(button);
+    contenr.appendChild(li);
+    
+        inp.value = "";
+}
